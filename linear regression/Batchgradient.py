83f26bff5198d7097925cc64c21beb520f8705d4
Vincent Jouanneau <63442366+VincentJouanneau@users.noreply.github.com>
2023-04-09 12:29:37 +0200

    feat: Update Batchgradient.py

--- linear regression/Batchgradient.py
+++ linear regression/Batchgradient.py
@@ -1,20 +1,15 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-###calcul de l'erreur
+#--Calcul de la fonction coût--#
 def fonction_coût (X,y,theta): 
     
     J = (1 / (2 * X.shape[0])) * np.sum((np.dot(X,theta)-y)**2)
     return J
         
-###Algo gradient (reajustement des paramètre)
+#--Algo gradient (reajustement des paramètres)--#
 
 def batch_gradient (X,y,theta, alpha) :
     l = np.dot(np.transpose(X),(np.dot(X,theta) - y))
@@ -22,12 +17,14 @@ def batch_gradient (X,y,theta, alpha) :
     return theta - alpha*grad
 
     
-
+#---Importation du fichier Excel---#
 house_data = pd.read_csv('house.csv')
 
 house_data = house_data[house_data['loyer'] < 10000]
 #plt.plot(house_data['surface'], house_data['loyer'], 'ro', markersize=4)
 #plt.show()
+
+#---Intialisation---#
 
 X = np.array([np.ones(house_data.shape[0]), house_data['surface'].values]).T
 y = np.array(house_data['loyer'])
@@ -39,21 +36,22 @@ e = 0
 iteration = 0
 E0 = 10
 
+#Paramètres générés aleatoirement 
 theta = np.random.rand(2,1)
 
-#v = (np.dot(X, theta) - y)**2
+#Calcul à l'iteration 0 de la fonction coût
 J = fonction_coût(X, y, theta)
 
-theta = batch_gradient (X,y,theta,alpha);
-
+#---Demarage de l'algo: on cherche à faire converger la fonction coût vers sont minimun---#
 while (abs(J - e) > epsilon) :
     iteration =+ 1
     
     theta = batch_gradient (X,y,theta,alpha);
     e = J
     J = fonction_coût(X, y, theta)
-    print (J-e)
-    
+    #print (J-e)
+
+#---Affichage---#
 plt.plot(X[:, 1], y, 'ro', markersize=4)
 
 x_values = [np.min(X[:, 1]), np.max(X[:, 1])]
